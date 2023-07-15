d6a4f291af13bd097fb810911264f6a3188b3b3d
decimag <christophe.magnoux@decimag.fr>
2023-07-15 22:16:31 +0200

    new

--- MageneProject/settings.py
+++ MageneProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rn+st873f6ib&)o3-&wcx9)$k!6!23^ejldfukjc#!vqsfv=bq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["fourretout.net", "localhost", "127.0.0.1"]
 
 
 # Application definition
